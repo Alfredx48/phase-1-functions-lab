c3e6c95037478f3cccbfc12092483b0a5454db42
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-13 15:24:29 -0700

    commit tree f19f79f8f34edc360dfb6c3d794dbaa10de755bd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(hqdistance){
+    if (hqdistance >= 42){
+        return hqdistance - 42;
+    } else { 
+        return 42 - hqdistance;
+    }
+  
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet)*264
+ }
+
+
+function distanceTravelledInFeet(start, end){
+    return distanceFromHqInFeet(end) - distanceFromHqInFeet(start);
+}
+
+function calculatesFarePrice(start, end){
+    let feet = distanceTravelledInFeet(start, end)
+    if (feet <= 400) {
+        return 0;
+    }else if (feet >= 400 && feet < 2000){
+        return .02 * (feet - 400);
+    }else if (feet >= 2000 &&  feet < 2500) {
+        return 25;
+    }else {
+        return 'cannot travel that far'
+    }
+}
